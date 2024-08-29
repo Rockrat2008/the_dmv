e2274f3f620d61fd8a0a7bfe352f2d74bc33c20d
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-29 18:32:02 -0400

    Creates the registrant.rb file and the method for the initialization test

---
+++ lib/registrant.rb
@@ -0,0 +1,29 @@
+class Registrant
+    attr_reader :name,
+                :age,
+                :permit,
+                :license_data
+
+    def initialize(name, age, license_data, permit: false)
+        @name = name
+        @age = age
+        @permit = permit
+        @license_data = license_data
+    end
+
+    def name
+        @name
+    end
+
+    def age
+        @age
+    end
+
+    def permit?
+        @permit
+    end
+
+    def license_data
+        @license_data
+    end
+end
