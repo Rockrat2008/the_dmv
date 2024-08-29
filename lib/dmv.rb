7279a136ce741b3a4ecf63fec06242dbd255abb3
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-29 16:29:56 -0400

    Updatse dmv.rb so all methods are written and pass the required tests

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -4,12 +4,16 @@ class Dmv
     @facilities = []
   end
 
+  def facilities
+    @facilities
+  end
+
   def add_facility(facility)
     @facilities << facility
   end
 
   def facilities_offering_service(service)
-    @facilities.find do |facility|
+    @facilities.find_all do |facility|
       facility.services.include?(service)
     end
   end
