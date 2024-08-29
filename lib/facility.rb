56eb1cbed9e22eb3cc621bf35660ca0bd9381ef0
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-29 16:19:38 -0400

    Creates facility.rb file with required methods

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,15 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility_specs)
+    @name = facility_specs[:name]
+    @address = facility_specs[:address]
+    @phone = facility_specs[:phone]
     @services = []
+    # require 'pry'; binding.pry
   end
 
-  def add_services(service)
+  def add_service(service)
     @services << service
   end
 end
