ba454cbcd3aeebea95e7140af3a374052929f3a4
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-01 18:06:10 -0400

    Added registration date and plate type for registering vehicles

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -6,7 +6,10 @@ class Vehicle
               :make,
               :model,
               :engine,
-              :registration_date
+              :registration_date,
+              :plate_type
+  attr_writer :registration_date,
+              :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -14,11 +17,12 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
-    @registration_date = vehicle_details[:registration_date]
+    @registration_date = nil
+    @plate_type = nil
   end
 
   def antique?
-    Date.today.year - @year > 25
+    Date.today.year - @year >= 25
   end
 
   def electric_vehicle?
