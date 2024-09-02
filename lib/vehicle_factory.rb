471b77399f6df9f6e3f644fca31f069777303531
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-02 12:50:27 -0400

    Adds the method for Create Vehicles and confirms vehicles are created test passes

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,6 +1,12 @@
 class VehicleFactory
 
-    def create_vehicles
-
+    def initialize
+        @wa_vehicles = []
+    end
+        
+    def create_vehicles(vehicles)
+        vehicles.map do |vehicle|
+            @wa_vehicles << Vehicle.new(engine: :ev, make: vehicle[:make], model: vehicle[:model], plate_type: nil, registration_date: nil, vin: vehicle[:vin_1_10], year: vehicle[:model_year])
+        end
     end
 end
