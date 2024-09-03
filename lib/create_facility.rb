4edffaa881f6e5f512717ac094905e68da97610a
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-03 15:34:21 -0400

    Cleans up and improves formatting of DMV address field

--- lib/create_facility.rb
+++ lib/create_facility.rb
@@ -8,12 +8,15 @@ class CreateFacility
         facility_info.map do |facility|
           if facility[:state] == "CO"
             combined_addr = [facility[:address_li], facility[:address__1], facility[:city], facility[:state], facility[:zip]]
+            combined_addr = combined_addr.join(" ")
             @facility_info << Facility.new(name: facility[:dmv_office], address: combined_addr, phone: facility[:phone])
           elsif facility[:state] == "NY"
             combined_addr = [facility[:street_address_line_1], facility[:street_address_line_2], facility[:city], facility[:state], facility[:zip_code]]
+            combined_addr = combined_addr.join(" ")
             @facility_info << Facility.new(name: facility[:office_name], address: combined_addr, phone: facility[:public_phone_number])
           elsif facility[:state] == "MO"
             combined_addr = [facility[:address1], facility[:city], facility[:state], facility[:zipcode]]
+            combined_addr = combined_addr.join(" ")
             @facility_info << Facility.new(name: facility[:name], address: combined_addr, phone: facility[:phone])
           else
             puts "Not a vailid file"
