80be35ba88e2fba67b423f06723ad57a0c5bce1b
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-02 20:08:53 -0400

    Creates the DMV facility locations for CO, NY, and MO

---
+++ lib/create_facility.rb
@@ -0,0 +1,24 @@
+class CreateFacility
+
+    def initialize
+        @facility_info = []
+    end
+
+    def create_facilities(facility_info)
+        facility_info.map do |facility|
+          if facility[:state] == "CO"
+            combined_addr = [facility[:address_li], facility[:address__1], facility[:city], facility[:state], facility[:zip]]
+            @facility_info << Facility.new(name: facility[:dmv_office], address: combined_addr, phone: facility[:phone])
+          elsif facility[:state] == "NY"
+            combined_addr = [facility[:street_address_line_1], facility[:street_address_line_2], facility[:city], facility[:state], facility[:zip_code]]
+            @facility_info << Facility.new(name: facility[:office_name], address: combined_addr, phone: facility[:public_phone_number])
+          elsif facility[:state] == "MO"
+            combined_addr = [facility[:address1], facility[:city], facility[:state], facility[:zipcode]]
+            @facility_info << Facility.new(name: facility[:name], address: combined_addr, phone: facility[:phone])
+          else
+            puts "Not a vailid file"
+          end
+        end
+        require 'pry'; binding.pry
+    end
+end
