933a9a551f51799eb4bda4993f07d6b826a107b8
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-02 20:17:17 -0400

    Fixed typos

--- spec/create_facility_spec.rb
+++ spec/create_facility_spec.rb
@@ -12,21 +12,21 @@ RSpec.describe CreateFacility do
     end
 
     describe '#co_dmv_office_locations' do
-        it 'can create co dmv office locations' do
+        it 'can create the co dmv office locations' do
             co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
             @new_facility.create_facilities(co_dmv_office_locations)
         end
     end
 
     describe '#ny_dmv_office_locations' do
-        it 'can create ny dmv office locations' do
+        it 'can create the ny dmv office locations' do
             ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
             @new_facility.create_facilities(ny_dmv_office_locations)
         end
     end
 
     describe '#mo_dmv_office_locations' do
-        it 'can create mo dmv office locations' do
+        it 'can create the mo dmv office locations' do
             mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
             @new_facility.create_facilities(mo_dmv_office_locations)
         end
